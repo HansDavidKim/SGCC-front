267372fcbf5acf8df14ed1ed614395f7d85e61ba
jiy2745 <jiy2745@gmail.com>
2025-08-09 15:48:47 +0900

    fix: /community/feed의 파라미터가 적절히 변경되도록 수정
    - 사용자가 파라미터에 소수나 문자열 등을 넣었을 때 적절하게 정수로 변경되도록 수정

--- src/routes/community/feed/+page.server.ts
+++ src/routes/community/feed/+page.server.ts
@@ -6,13 +6,14 @@ import type { PageServerLoad } from "./$types";
 const POSTS_PER_PAGE = 10;
 
 export const load: PageServerLoad = async ({ url }) => {
-    const currentPage = parseInt(url.searchParams.get("page") || "1");
+    const pageParam = url.searchParams.get("page");
+    const currentPage = parseInt(pageParam) || 1;
     const totalPages = Math.ceil(allPosts.length / POSTS_PER_PAGE);
-    const offset = (currentPage - 1) * POSTS_PER_PAGE;
-    const slicedPostSummary: PostSummary[] = allPosts.slice(
-        offset,
-        offset + POSTS_PER_PAGE,
-    );
+
+    if (Number(pageParam) != currentPage) {
+        url.searchParams.set("page", currentPage);
+        throw redirect(307, url.toString());
+    }
 
     if (currentPage < 1) {
         url.searchParams.set("page", "1");
@@ -21,6 +22,12 @@ export const load: PageServerLoad = async ({ url }) => {
         url.searchParams.set("page", totalPages.toString());
         throw redirect(307, url.toString());
     }
+    
+    const offset = (currentPage - 1) * POSTS_PER_PAGE;
+    const slicedPostSummary: PostSummary[] = allPosts.slice(
+        offset,
+        offset + POSTS_PER_PAGE,
+    );
 
-    return { slicedPostSummary, currentPage, totalPages };
+    return { currentPage, totalPages, slicedPostSummary };
 };
