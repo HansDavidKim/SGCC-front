528a5c5ac49049e4860908a527abb18f16a0dcc5
jiy2745 <jiy2745@gmail.com>
2025-09-01 16:01:02 +0900

    feat: SNS 페이지 깃허브 추가
    - SNS 페이지에 깃허브를 추가함

--- src/routes/aboutus/SNS/+page.svelte
+++ src/routes/aboutus/SNS/+page.svelte
@@ -13,35 +13,72 @@
       공식 GITHUB ORGANIZATION을 통해 동아리 내에서 진행한 프로젝트 내용을 확인해볼 수 있습니다.<br>
       지금까지 진행된 슬기로운 인디 생활 등 지금까지 진행된 다양한 프로젝트들의 소스 코드를 통해 앞으로의 개발에 있어서의 지침서로 사용할 수 있습니다.<br>
     </p>
-    <a 
-      href="https://www.instagram.com/sgcc_sogang" 
-      target="_blank"
-      class="
-        block w-75 max-w-full font-sans text-black no-underline
-        rounded-lg bg-zinc-50 p-5 my-15
-        shadow-md transition-transform duration-200 ease-in-out 
-        hover:-translate-y-1
-      "
-    >
-      <div class="flex items-center justify-between mb-4">
-        <img 
-          src="/src/lib/assets/images/SGCC-logo.jpg" 
-          alt="SGCC Logo" 
-          class="w-20 h-20 rounded-full object-cover mr-5 border-2 border-gray-200 p-0.5"
-        >
-        <div class="flex flex-col mx-auto">
-          <p class="text-xl font-semibold mb-1.5">sgcc_sogang</p>
-          <div class="text-sm font-bold text-white bg-sky-500 rounded-md py-1 px-3 text-center">
-            프로필 보기
+
+    <div class="flex justify-between">
+      <a 
+        href="https://www.instagram.com/sgcc_sogang" 
+        target="_blank"
+        class="
+          block w-75 h-50 max-w-full font-sans text-black no-underline
+          rounded-lg bg-zinc-50 p-5 mx-12 my-21
+          shadow-md transition-transform duration-200 ease-in-out 
+          hover:-translate-y-1
+        "
+      >
+        <div class="flex items-center justify-between mb-4">
+          <img 
+            src="/src/lib/assets/images/SGCC-logo.jpg" 
+            alt="SGCC Logo" 
+            class="w-20 h-20 rounded-full object-cover mr-5 border-2 border-gray-200 p-0.5"
+          >
+          <div class="flex flex-col mx-auto">
+            <p class="text-xl font-semibold mb-1.5">sgcc_sogang</p>
+            <div class="text-sm font-bold text-white bg-sky-500 rounded-md py-1 px-3 text-center">
+              프로필 보기
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="text-sm leading-snug">
-        <p class="font-semibold">&lt;SGCC&gt;</p>
-        <p>서강대학교 중앙 컴퓨터 동아리</p>
-        <p>Sogang Computer Club [since 1982]</p>
-      </div>
-    </a>
+        <div class="text-sm leading-snug">
+          <p class="font-semibold">&lt;SGCC&gt;</p>
+          <p>서강대학교 중앙 컴퓨터 동아리</p>
+          <p>Sogang Computer Club [since 1982]</p>
+        </div>
+      </a>
+
+      <a
+        href="https://github.com/Sogang-Computer-Club"
+        target="_blank"
+        class="
+          block w-75 max-w-full font-sans no-underline
+          rounded-xl bg-slate-800 p-6 shadow-lg m-12 my-18
+          transition-transform hover:-translate-y-1
+        "
+      >
+        <div class="relative mx-auto mb-5 w-max">
+          <img
+            src="/src/lib/assets/images/SGCC-logo.jpg"
+            alt="SGCC Logo"
+            class="h-36 w-36 rounded-full border-2 border-slate-700 object-cover"
+          />
+        </div>
+
+        <div class="mb-6 text-center">
+          <h2 class="text-2xl font-bold text-white">SGCC</h2>
+          <p class="text-md text-slate-400">Sogang-Computer-Club</p>
+        </div>
+
+        <div
+          class="
+            w-full rounded-lg bg-slate-700 py-2.5
+            text-center text-md font-semibold text-slate-300
+            border-1 border-slate-600
+            transition-colors group-hover:bg-slate-600 group-hover:text-white
+          "
+        >
+          Follow
+        </div>
+      </a>
+    </div>
   </div>
 </main>
