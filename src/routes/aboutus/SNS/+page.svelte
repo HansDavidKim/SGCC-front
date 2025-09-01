fd1f217a052bdf6895df314f2df7ab6ac9712e85
jiy2745 <jiy2745@gmail.com>
2025-09-01 15:16:32 +0900

    feat: 인스타그램 SNS 추가
    - 인스타그램 SNS를 추가함

--- src/routes/aboutus/SNS/+page.svelte
+++ src/routes/aboutus/SNS/+page.svelte
@@ -4,20 +4,44 @@
 </script>
 
 <main>
-
   <div class="flex flex-col py-20 relative items-center bg-black h-[1487px] text-white">
     <h2 class="text-[50px] font-semibold text-center">SNS</h2>
-    <p class="absolute top-[198px] text-[20px] leading-[40px] font-light text-center">
+    <p class="text-[20px] leading-[40px] font-light text-center">
       SGCC는 INSTAGRAM 공식 계정을 통해 다양한 활동 내용들을 SNS를 통해 공유하고 있습니다.<br>
       이외에도 신규 회원 모집 및 예정된 활동 등 여러 공지 사항 또한 전달하고 있습니다.<br>
       <br>
       공식 GITHUB ORGANIZATION을 통해 동아리 내에서 진행한 프로젝트 내용을 확인해볼 수 있습니다.<br>
       지금까지 진행된 슬기로운 인디 생활 등 지금까지 진행된 다양한 프로젝트들의 소스 코드를 통해 앞으로의 개발에 있어서의 지침서로 사용할 수 있습니다.<br>
     </p>
+    <a 
+      href="https://www.instagram.com/sgcc_sogang" 
+      target="_blank"
+      class="
+        block w-75 max-w-full font-sans text-black no-underline
+        rounded-lg bg-zinc-50 p-5 my-15
+        shadow-md transition-transform duration-200 ease-in-out 
+        hover:-translate-y-1
+      "
+    >
+      <div class="flex items-center justify-between mb-4">
+        <img 
+          src="/src/lib/assets/images/SGCC-logo.jpg" 
+          alt="SGCC Logo" 
+          class="w-20 h-20 rounded-full object-cover mr-5 border-2 border-gray-200 p-0.5"
+        >
+        <div class="flex flex-col mx-auto">
+          <p class="text-xl font-semibold mb-1.5">sgcc_sogang</p>
+          <div class="text-sm font-bold text-white bg-sky-500 rounded-md py-1 px-3 text-center">
+            프로필 보기
+          </div>
+        </div>
+      </div>
 
-    <div class="absolute top-[467px] bg-[#AE1F1F] w-[1270px] h-[955px] rounded-[50px]">
-
-    </div>
+      <div class="text-sm leading-snug">
+        <p class="font-semibold">&lt;SGCC&gt;</p>
+        <p>서강대학교 중앙 컴퓨터 동아리</p>
+        <p>Sogang Computer Club [since 1982]</p>
+      </div>
+    </a>
   </div>
-
 </main>
