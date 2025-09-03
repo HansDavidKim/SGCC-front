2871b951dc1e0057d723f641e48390f85fc67020
jiy2745 <jiy2745@gmail.com>
2025-09-03 13:18:29 +0900

    fix: 모바일 화면에서 SNS가 제대로 보이지 않던 문제 수정
    - 모바일 화면을 반응형으로 변경

--- src/routes/aboutus/SNS/+page.svelte
+++ src/routes/aboutus/SNS/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-  <div class="relative flex flex-col items-center h-210 desktop:h-[1010px] px-5 py-22.5 desktop:py-21.5 text-white bg-black">
+  <div class="relative flex flex-col items-center desktop:h-[1010px] px-5 py-22.5 desktop:py-21.5 text-white bg-black">
     <h2 class="mb-16.5 desktop:mb-17.5 text-[35px] desktop:text-[50px] text-center">SNS</h2>
     <!-- Desktop Text -->
     <p class="mb-26 text-[20px] font-light leading-[30px] desktop:leading-[40px] text-center hidden tablet:block">
@@ -14,13 +14,22 @@
       지금까지 진행된 슬기로운 인디 생활 등 지금까지 진행된 다양한 프로젝트들의 소스 코드를 통해 앞으로의 개발에 있어서의 지침서로 사용할 수 있습니다.<br>
     </p>
 
-    <div class="flex justify-between">
+    <!-- Mobile Text -->
+    <p class="tablet:hidden mb-6 text-[20px] font-light leading-[30px] desktop:leading-[40px] text-left">
+      SGCC는 INSTAGRAM 공식 계정을 통해 다양한 활동 내용들을 SNS를 통해 공유하고 있습니다.
+      이외에도 신규 회원 모집 및 예정된 활동 등 여러 공지 사항 또한 전달하고 있습니다.<br>
+      <br>
+      공식 GITHUB ORGANIZATION을 통해 동아리 내에서 진행한 프로젝트 내용을 확인해볼 수 있습니다.
+      지금까지 진행된 슬기로운 인디 생활 등 지금까지 진행된 다양한 프로젝트들의 소스 코드를 통해 앞으로의 개발에 있어서의 지침서로 사용할 수 있습니다.<br>
+    </p>
+
+    <div class="flex flex-col items-center justify-center tablet:flex-row px-auto w-full">
       <a 
         href="https://www.instagram.com/sgcc_sogang" 
         target="_blank"
         class="
           block w-75 h-50 max-w-full font-sans text-black no-underline
-          rounded-lg bg-zinc-50 p-5 mx-12 my-21
+          rounded-lg bg-zinc-50 p-5 mx-auto my-auto
           shadow-md transition-transform duration-200 ease-in-out 
           hover:-translate-y-1
         "
@@ -51,7 +60,7 @@
         target="_blank"
         class="
           block w-75 max-w-full font-sans no-underline
-          rounded-xl bg-slate-800 p-6 shadow-lg m-12 my-18
+          rounded-xl bg-slate-800 p-6 shadow-lg mx-auto my-6
           transition-transform hover:-translate-y-1
         "
       >
@@ -79,13 +88,5 @@
           Follow
         </div>
       </a>
-    <!-- Mobile Text -->
-    <p class="tablet:hidden mb-6 text-[20px] font-light leading-[30px] desktop:leading-[40px] text-left">
-      SGCC는 INSTAGRAM 공식 계정을 통해 다양한 활동 내용들을 SNS를 통해 공유하고 있습니다.
-      이외에도 신규 회원 모집 및 예정된 활동 등 여러 공지 사항 또한 전달하고 있습니다.<br>
-      <br>
-      공식 GITHUB ORGANIZATION을 통해 동아리 내에서 진행한 프로젝트 내용을 확인해볼 수 있습니다.
-      지금까지 진행된 슬기로운 인디 생활 등 지금까지 진행된 다양한 프로젝트들의 소스 코드를 통해 앞으로의 개발에 있어서의 지침서로 사용할 수 있습니다.<br>
-    </p>
   </div>
 </main>
